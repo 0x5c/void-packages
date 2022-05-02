71a12eef16c999f71c74b086c8313b76f9fe1b67
0x5c <dev@0x5c.io>
2022-05-02 02:25:21 -0400

    borkborkborkborkborkborkborkborkborkborkborkborkbork f

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -11,19 +11,8 @@ common/scripts/lint-commits $base $tip || EXITCODE=$?
 
 for t in $(awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates); do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
-	xlint "$t" > /tmp/lint_out || EXITCODE=$?
-#	if [ "$?" -ne 0 ]; then
-		awk '{
-			split($0, a, ": ")
-			split(a[1], b, ":")
-			msg = substr($0, index($0, ": ") + 2)
-			if (b[2]) {
-				line = ",line=" b[2]
-			}
-			print $0
-			printf "::error title=xlint,file=%s%s::%s\n", b[1], line, msg
-		}' /tmp/lint_out
-#	fi
-	common/scripts/lint-version-change "$t" $base $tip || EXITCODE=$?
+	xlint "$t" > /tmp/xlint_out || EXITCODE=$?
+	common/scripts/lint-version-change "$t" $base $tip > /tmp/vlint_out || EXITCODE=$?
+	awk -f common/scripts/lint2annotations.awk /tmp/xlint_out /tmp/vlint_out
 done
 exit $EXITCODE
